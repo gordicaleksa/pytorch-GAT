eec6b19970525007235954cb8610e426aaad20fe
Aleksa Gordic <gordicaleksa@gmail.com>
2020-12-30 20:53:42 +0100

    Add skeleton for method #3 - scatter-based

--- models/definitions/GAT.py
+++ models/definitions/GAT.py
@@ -49,15 +49,18 @@ class GATLayerImp3(torch.nn.Module):
     Implementation #3 was inspired by PyTorch Geometric: https://github.com/rusty1s/pytorch_geometric
     """
 
+    source_nodes_dim = 0  # position of source nodes in edge index
+    target_nodes_dim = 1  # position of target nodes in edge index
+
     def __init__(self, num_in_features, num_out_features, num_of_heads, concat=True, activation=nn.ELU,
-                 dropout_prob=0.6, add_self_loops=True, bias=True, log_attention_weights=False):
+                 dropout_prob=0.6, add_skip_connection=True, bias=True, log_attention_weights=False):
 
         super().__init__()
 
         self.num_of_heads = num_of_heads
         self.num_out_features = num_out_features
         self.concat = concat  # whether we should concatenate or average the attention heads
-        self.add_self_loops = add_self_loops
+        self.add_skip_connection = add_skip_connection
 
         #
         # Trainable weights: linear projection matrix (denoted as "W" in the paper),
@@ -84,6 +87,7 @@ class GATLayerImp3(torch.nn.Module):
         # End of trainable weights
         #
 
+        self.leakyReLU = nn.LeakyReLU(0.2)  # no need to expose everything
         self.activation = activation
         self.dropout = nn.Dropout(p=dropout_prob)  # Used in 3 locations, feature matrix before/after proj and attention
 
@@ -106,40 +110,63 @@ class GATLayerImp3(torch.nn.Module):
         nn.init.xavier_uniform_(self.scoring_fn_source)
         torch.nn.init.zeros_(self.bias)
 
-    def forward(self, nodes_features_matrix, edge_index):
+    def lift(self, scores_source, scores_target, nodes_features_matrix_proj, edge_index):
+        dim = 0
+        # todo: try normal indexing without index select
+        scores_source = scores_source.index_select(dim, edge_index[self.source_nodes_dim])
+        scores_target = scores_target.index_select(dim, edge_index[self.target_nodes_dim])
+        nodes_features_matrix_proj_lifted = nodes_features_matrix_proj.index_select(dim, edge_index[self.source_nodes_dim])
+
+        return scores_source, scores_target, nodes_features_matrix_proj_lifted
+
+    def broadcast(self, this, other):
+        for _ in range(this.dim(), other.dim()):
+            this = this.unsqueeze(-1)
+        this = this.expand_as(other)
+        return this
+
+    def forward(self, in_nodes_features, edge_index):
+        num_of_nodes = in_nodes_features.shape[0]
         # Shape = [N, FIN] where N - number of nodes in the graph, FIN number of input features per node
         # We apply the dropout to all of the input node features (as mentioned in the paper)
-        nodes_features_matrix = self.dropout(nodes_features_matrix)
+        in_nodes_features = self.dropout(in_nodes_features)
 
         # Shape = [N, NH, FOUT] where NH - number of heads, FOUT number of output features per head
         # We project the input node features into NH independent output features (one for each attention head)
-        nodes_features_matrix_proj = self.linear_proj(nodes_features_matrix).view(-1, self.num_of_heads, self.num_out_features)
+        nodes_features_proj = self.linear_proj(in_nodes_features).view(-1, self.num_of_heads, self.num_out_features)
 
-        scores_target = (nodes_features_matrix_proj * self.scoring_fn_target).sum(dim=-1)
-        scores_source = (nodes_features_matrix_proj * self.scoring_fn_source).sum(dim=-1)
+        nodes_features_proj = self.dropout(nodes_features_proj)  # in the official GAT imp they did dropout here as well
 
-        if self.add_self_loops:  # Check out how Petar managed the data
-            edge_index, _ = remove_self_loops(edge_index)
-            edge_index, _ = add_self_loops(edge_index, num_nodes=num_nodes)
+        scores_source = (nodes_features_proj * self.scoring_fn_source).sum(dim=-1)
+        scores_target = (nodes_features_proj * self.scoring_fn_target).sum(dim=-1)
 
-        # expand this part
-        out = self.propagate(edge_index, x=(x_l, x_r), alpha=(alpha_l, alpha_r), size=size)
+        scores_source_lifted, scores_target_lifted, nodes_features_proj_lifted = self.lift(scores_source, scores_target, nodes_features_proj, edge_index)
+        scores_per_edge = self.leakyReLU(scores_source_lifted + scores_target_lifted)
+
+        attentions_per_edge = self.scatter_softmax(scores_per_edge, edge_index[self.target_nodes_dim])
+        # Add stochasticity to neighborhood aggregation
+        attentions_per_edge = self.dropout(attentions_per_edge)  # todo: check whether it's ok to use the same dropout
+
+        # Element-wise (aka Hadamard) product. Operator * does the same thing as torch.mul
+        nodes_features_proj_lifted_weighted = nodes_features_proj_lifted * attentions_per_edge
+
+        # This part adds up weighted, projected neighborhoods for every target node
+        out_nodes_features = torch.zeros(num_of_nodes, dtype=in_nodes_features.dtype, device=in_nodes_features.device)
+        index = self.broadcast(edge_index[self.target_nodes_dim], nodes_features_proj_lifted_weighted)
+        out_nodes_features.scatter_add_(0, index, nodes_features_proj_lifted_weighted)
 
         if self.log_attention_weights:
-            print('todo')
-            # self.attention_weights =
-
-        # todo: add residual/skip connection
+            self.attention_weights = attentions_per_edge
 
         if self.concat:
-            out = out.view(-1, self.heads * self.out_channels)
+            out_nodes_features = out_nodes_features.view(-1, self.num_of_heads * self.num_out_features)
         else:
-            out = out.mean(dim=1)
+            out_nodes_features = out_nodes_features.mean(dim=1)
 
         if self.bias is not None:
-            out += self.bias
+            out_nodes_features += self.bias
 
-        return self.activation(out)
+        return self.activation(out_nodes_features)
 
 
 # Adapted from the official GAT implementation
